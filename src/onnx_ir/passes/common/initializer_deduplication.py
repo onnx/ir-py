51de22672d099e6b4af9c001cf197bc46c75ee29
Justin Chu <justinchuby@users.noreply.github.com>
2025-08-18 17:57:08 -0700

    [pass] Deduplicate initializers in subgraphs
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- src/onnx_ir/passes/common/initializer_deduplication.py
+++ src/onnx_ir/passes/common/initializer_deduplication.py
@@ -18,9 +18,6 @@ class DeduplicateInitializersPass(ir.passes.InPlacePass):
     This pass detects initializers with identical shape, dtype, and content,
     and replaces all duplicate references with a canonical one.
 
-    To deduplicate initializers from subgraphs, use :class:`~onnx_ir.passes.common.LiftSubgraphInitializersToMainGraphPass`
-    to lift the initializers to the main graph first before running pass.
-
     .. versionadded:: 0.1.3
     """
 
@@ -29,28 +26,30 @@ class DeduplicateInitializersPass(ir.passes.InPlacePass):
         self.size_limit = size_limit
 
     def call(self, model: ir.Model) -> ir.passes.PassResult:
-        graph = model.graph
-        initializers: dict[tuple[ir.DataType, tuple[int, ...], bytes], ir.Value] = {}
         modified = False
 
-        for initializer in tuple(graph.initializers.values()):
-            # TODO(justinchuby): Handle subgraphs as well. For now users can lift initializers
-            # out from the main graph before running this pass.
-            const_val = initializer.const_value
-            if const_val is None:
-                # Skip if initializer has no constant value
-                continue
+        for graph in model.graphs():
+            initializers: dict[tuple[ir.DataType, tuple[int, ...], bytes], ir.Value] = {}
+            for initializer in tuple(graph.initializers.values()):
+                if initializer.is_graph_input() or initializer.is_graph_output():
+                    # Skip graph inputs and outputs
+                    continue
 
-            if const_val.size > self.size_limit:
-                continue
+                const_val = initializer.const_value
+                if const_val is None:
+                    # Skip if initializer has no constant value
+                    continue
 
-            key = (const_val.dtype, tuple(const_val.shape), const_val.tobytes())
-            if key in initializers:
-                modified = True
-                ir.convenience.replace_all_uses_with(initializer, initializers[key])  # type: ignore[index]
-                assert initializer.name is not None
-                graph.initializers.pop(initializer.name)
-            else:
-                initializers[key] = initializer  # type: ignore[index]
+                if const_val.size > self.size_limit:
+                    continue
+
+                key = (const_val.dtype, tuple(const_val.shape), const_val.tobytes())
+                if key in initializers:
+                    modified = True
+                    ir.convenience.replace_all_uses_with(initializer, initializers[key])  # type: ignore[index]
+                    assert initializer.name is not None
+                    graph.initializers.pop(initializer.name)
+                else:
+                    initializers[key] = initializer  # type: ignore[index]
 
         return ir.passes.PassResult(model=model, modified=modified)
