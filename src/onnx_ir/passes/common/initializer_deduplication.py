7b10221675bbb6ee0759ef8a1adb2324030a751f
Justin Chu <justinchuby@users.noreply.github.com>
2025-08-18 18:46:35 -0700

    DeduplicateHashedInitializersPass
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- src/onnx_ir/passes/common/initializer_deduplication.py
+++ src/onnx_ir/passes/common/initializer_deduplication.py
@@ -4,7 +4,7 @@
 
 from __future__ import annotations
 
-__all__ = ["DeduplicateInitializersPass", "DeduplicateBigInitializersPass"]
+__all__ = ["DeduplicateInitializersPass", "DeduplicateHashedInitializersPass"]
 
 
 import hashlib
@@ -63,16 +63,13 @@ class DeduplicateInitializersPass(ir.passes.InPlacePass):
         return ir.passes.PassResult(model=model, modified=modified)
 
 
-class DeduplicateBigInitializersPass(ir.passes.InPlacePass):
-    """Remove big, duplicated initializer tensors from the graph.
+class DeduplicateHashedInitializersPass(ir.passes.InPlacePass):
+    """Remove duplicated initializer tensors (using a hashed method) from the graph.
 
     This pass detects initializers with identical shape, dtype, and hashed content,
     and replaces all duplicate references with a canonical one.
 
-    To deduplicate initializers from subgraphs, use :class:`~onnx_ir.passes.common.LiftSubgraphInitializersToMainGraphPass`
-    to lift the initializers to the main graph first before running pass.
-
-    .. versionadded:: 0.1.5
+    .. versionadded:: 0.1.7
     """
 
     def __init__(self, size_limit: int = 4 * 1024 * 1024 * 1024):
@@ -81,47 +78,48 @@ class DeduplicateBigInitializersPass(ir.passes.InPlacePass):
         self.size_limit = size_limit
 
     def call(self, model: ir.Model) -> ir.passes.PassResult:
-        graph = model.graph
-        initializers: dict[tuple[ir.DataType, tuple[int, ...], str], ir.Value] = {}
         modified = False
 
-        for initializer in tuple(graph.initializers.values()):
-            const_val = initializer.const_value
-            if const_val is None:
-                # Skip if initializer has no constant value
-                continue
+        for graph in model.graphs():
+            initializers: dict[tuple[ir.DataType, tuple[int, ...], str], ir.Value] = {}
 
-            if const_val.size > self.size_limit:
-                continue
-
-            if const_val.dtype == ir.DataType.STRING:
-                # Skip string initializers as they don't have a bytes representation
-                continue
-
-            # Hash tensor data to avoid storing large amounts of data in memory
-            hashed = hashlib.sha512()
-            tensor_data = const_val.numpy()
-            hashed.update(tensor_data)
-            tensor_digest = hashed.hexdigest()
-
-            tensor_dims = tuple(const_val.shape.numpy())
-
-            key = (const_val.dtype, tensor_dims, tensor_digest)
-
-            if key in initializers:
-                if initializers[key].const_value.tobytes() != const_val.tobytes():
-                    logger.warning(
-                        "Initializer deduplication failed: "
-                        "hashes match but values differ with values %s and %s",
-                        initializers[key],
-                        initializer,
-                    )
+            for initializer in tuple(graph.initializers.values()):
+                const_val = initializer.const_value
+                if const_val is None:
+                    # Skip if initializer has no constant value
                     continue
-                modified = True
-                ir.convenience.replace_all_uses_with(initializer, initializers[key])  # type: ignore[index]
-                assert initializer.name is not None
-                graph.initializers.pop(initializer.name)
-            else:
-                initializers[key] = initializer  # type: ignore[index]
+
+                if const_val.size > self.size_limit:
+                    continue
+
+                if const_val.dtype == ir.DataType.STRING:
+                    # Skip string initializers as they don't have a bytes representation
+                    continue
+
+                # Hash tensor data to avoid storing large amounts of data in memory
+                hashed = hashlib.sha512()
+                tensor_data = const_val.numpy()
+                hashed.update(tensor_data)
+                tensor_digest = hashed.hexdigest()
+
+                tensor_dims = tuple(const_val.shape.numpy())
+
+                key = (const_val.dtype, tensor_dims, tensor_digest)
+
+                if key in initializers:
+                    if initializers[key].const_value.tobytes() != const_val.tobytes():
+                        logger.warning(
+                            "Initializer deduplication failed: "
+                            "hashes match but values differ with values %s and %s",
+                            initializers[key],
+                            initializer,
+                        )
+                        continue
+                    modified = True
+                    ir.convenience.replace_all_uses_with(initializer, initializers[key])  # type: ignore[index]
+                    assert initializer.name is not None
+                    graph.initializers.pop(initializer.name)
+                else:
+                    initializers[key] = initializer  # type: ignore[index]
 
         return ir.passes.PassResult(model=model, modified=modified)
